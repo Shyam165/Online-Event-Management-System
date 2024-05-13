c8f2df2ca2709493ff60f17223c2e258e38b1fc2
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-05-13 21:28:12 +0530

    adding files

---
+++ eventapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+admin.site.site_header = "Event Management System Admin"
+admin.site.site_title = "Event Management System Admin Portal"
+admin.site.index_title = "Welcome to Event Management System "
+
+urlpatterns = [
+    path('',views.index,name='index'),
+     path('about/',views.about,name='about'),
+      path('events/',views.events,name='events'),
+       path('booking/',views.booking,name='booking'),
+        path('contact/',views.contact,name='contact'),
+]
---
+++ eventapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from . models import Event
+from .forms import BookingForm
+from datetime import datetime
+from .models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def events(request):
+    dict_eve={
+        'eve':Event.objects.all()
+    }
+    return render(request,'events.html',dict_eve)
+def booking(request):
+    if request.method=='POST':
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    form=BookingForm()
+    dict_form={
+        'form':form
+    }
+    return render(request,'booking.html',dict_form)
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
